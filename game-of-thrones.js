8e0892a11be88c747555f41992fe41850fa41dd4
Josep Conejero <bermeu@gmail.com>
2022-01-24 21:21:21 +0100

    se añade game-of-thrones.js

---
+++ game-of-thrones.js
@@ -0,0 +1,5 @@
+const joffrey = new Rey("Joffrey", "Baratheon", 16, 2);
+const jaime = new Luchador("Jaime", "Lannister", 45, "Espada", 7);
+const daenerys = new Luchador("Daenerys", "Targaryen", 30, "Dragones", 10);
+const tyrion = new Asesor("Tyrion", "Lannister", 40, daenerys);
+const bronn = new Escudero("Bronn", "AguasNegras", 54, 0, jaime);
